a2e311de81e94e3b9a9e1849da92d1ccc8077249
KouKeioh <kou2013kou.kou@gmail.com>
2018-02-14 22:15:05 +0900

    3D空間に対応した音再生を追加。

--- sim_project/sound_system.cpp
+++ sim_project/sound_system.cpp
@@ -7,12 +7,14 @@ SoundSystem::SoundSystem()
 
 void SoundSystem::init()//初期化
 {
+	sound_r = 5.0f;
 	one_shot_flag = false;
 	sound_volume = 255;
 }
 
-void SoundSystem::LoadSound(char* path)//音を読み込む
+void SoundSystem::LoadSound(char* path, int data_flag)//音を読み込む
 {
+	SetCreate3DSoundFlag(data_flag);
 	sound_handl = LoadSoundMem(path);
 }
 
@@ -42,10 +44,68 @@ void SoundSystem::Play(float volume, int PlayType, bool copy_type)//音を再生する
 	}
 }
 
+void SoundSystem::Play3D(VECTOR pos, float volume, int PlayType, bool wire, bool copy_type)//音を再生する。(3D版)
+{
+	Set3DSoundListenerPosAndFrontPos_UpVecY(pos, VGet(0.0f, 1.0f, 0.0f));//リスナーを設定
+	Set3DRadiusSoundMem(sound_r, sound_handl);//聞こえる距離を設定
+	ChangeVolumeSoundMem(volume, sound_handl);//音量を設定。
+	ChangeVolumeSoundMem(volume, sound_copy_handl);//音量を設定。
+
+	if (wire == true)
+	{
+		DrawSphere3D(pos, 1.0f, 64, GetColor(150, 150, 150), GetColor(150, 150, 150), TRUE);
+	}
+
+	if (copy_type == false)
+	{
+		if (CheckSoundMem(sound_handl) == 0)//再生されていないなら
+		{
+			PlaySoundMem(sound_handl, PlayType, TRUE);//再生する。
+		}
+	}
+	else if (copy_type == true)//ハンドルが複製の場合
+	{
+		if (CheckSoundMem(sound_copy_handl) == 0)//再生されていないなら
+		{
+			PlaySoundMem(sound_copy_handl, PlayType, TRUE);//再生する。
+		}
+	}
+}
+
 void SoundSystem::OneShotPlay(float volume, int PlayType, bool copy_type)//一回だけ再生したいときはこっち
 {
 	ChangeVolumeSoundMem(volume, sound_handl);//音量を設定。
 	ChangeVolumeSoundMem(volume, sound_copy_handl);//音量を設定。
+
+	if (copy_type == false)
+	{
+		if ((CheckSoundMem(sound_handl) == 0) && one_shot_flag == false)//再生されていないなら
+		{
+			PlaySoundMem(sound_handl, PlayType, TRUE);//再生する。
+			one_shot_flag = true;
+		}
+	}
+	else if (copy_type == true)
+	{
+		if ((CheckSoundMem(sound_copy_handl) == 0) && one_shot_flag == false)//再生されていないなら
+		{
+			PlaySoundMem(sound_copy_handl, PlayType, TRUE);//再生する。
+			one_shot_flag = true;
+		}
+	}
+}
+
+void SoundSystem::OneShotPlay3D(VECTOR pos, float volume, int PlayType, bool wire, bool copy_type)//一回だけ再生したいときはこっち(3D版)
+{	
+	Set3DSoundListenerPosAndFrontPos_UpVecY(pos, VGet(0.0f, 1.0f, 0.0f));//リスナーを設定
+	Set3DRadiusSoundMem(sound_r, sound_handl);//聞こえる距離を設定
+	ChangeVolumeSoundMem(volume, sound_handl);//音量を設定。
+	ChangeVolumeSoundMem(volume, sound_copy_handl);//音量を設定。
+
+	if (wire == true)
+	{
+		DrawSphere3D(pos, 1.0f, 64, GetColor(150, 150, 150), GetColor(150, 150, 150), TRUE);
+	}
 
 	if (copy_type == false)
 	{
--- sim_project/sound_system.h
+++ sim_project/sound_system.h
@@ -10,19 +10,22 @@ public:
 
 	int sound_handl;
 	int sound_copy_handl;
+	float sound_r;
 
 	int sound_volume;
 	bool one_shot_flag;
 
 	void init();//初期化
 
-	void LoadSound(char* path);//音を読み込む
+	void LoadSound(char* path, int data_flag = FALSE);//音を読み込む(data_flagをTRUEにすると3D用ハンドルになります。)
 	void LoadSoundCopy(int copy_handl);//同じ音を使う場合はこちらの関数を使うこと。
 
 	void Play(float volume, int PlayType, bool copy_type = false);//音を再生する。
+	void Play3D(VECTOR pos, float volume, int PlayType, bool wire = false, bool copy_type = false);//音を再生する。(3D版)
 
 	void OneShotPlay(float volume, int PlayType, bool copy_type = false);//一回だけ再生したいときはこっち
-	void OneShotReset();//一回だけ再生した場合はこの関数で必ず初期化
+	void OneShotPlay3D(VECTOR pos, float volume, int PlayType, bool wire = false, bool copy_type = false);//一回だけ再生したいときはこっち(3D版)
+	void OneShotReset();//一回だけ再生した場合はこの関数で必ず初期化(3D兼用)
 
 	void Stop();//音を停止する。
 
