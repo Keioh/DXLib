50a61760b286e5b5695f238011a151a0d336a2a3
KouKeioh <kou2013kou.kou@gmail.com>
2018-02-24 00:39:01 +0900

    ファイルを追加しただけ

--- simProject/cpp/Scene/GameScene.cpp
+++ simProject/cpp/Scene/GameScene.cpp
@@ -65,7 +65,7 @@ void GameScene::DrawGameScene(int window_x, int window_y, Filer config, bool wir
 
 	camera.Init();//カメラを初期化
 
-	terrain.Transform(VGet(10.0f, 5.0f, 0.0f), 90, 0, 0, VGet(0, 0, 0));//３D空間に画像を表示
+	terrain.Transform(VGet(1.0f, 1.0f, 1.0f), 90, 0, 0, VGet(0, 0, 0));//３D空間に画像を表示
 	headquarters.Transform(VGet(0.1f, 0.1f, 0.1f), 0,0,0,VGet(0, 0.1,0));//本拠地の位置
 
 	while (game_scene_flag == 0 && ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0)
