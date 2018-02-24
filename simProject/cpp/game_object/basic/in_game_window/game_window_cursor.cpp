680c66eefa8dda3d6b26995de1aec1b39cb64121
KouKeioh <kou2013kou.kou@gmail.com>
2018-02-25 02:23:54 +0900

    更新

--- simProject/cpp/game_object/basic/in_game_window/game_window_cursor.cpp
+++ simProject/cpp/game_object/basic/in_game_window/game_window_cursor.cpp
@@ -2,7 +2,7 @@
 
 void GameWindowCursor::Init()
 {
-
+	int char_size = 8;//文字の大きさ
 }
 
 void GameWindowCursor::Load()
@@ -10,7 +10,15 @@ void GameWindowCursor::Load()
 
 }
 
-void GameWindowCursor::Draw(int pos_x, int pos_y, bool wire)
+void GameWindowCursor::Draw(int pos_x, int pos_y, char* strings_hitbox, char* strings, bool wire)
 {
+	if (hit_box.BoxColliderMouse(pos_x, strlen(strings_hitbox) * char_size, pos_y, 30, wire) == true)
+	{
+		DrawBox(hit_box.x, hit_box.y, hit_box.x + strlen(strings_hitbox) * char_size, hit_box.y + 30, GetColor(0, 0, 0), TRUE);
+
+		DrawFormatString(pos_x, pos_y, GetColor(0, 0, 0), "%s", strings);//カーソルヒット時の文字を表示
+	}
+
+	DrawFormatString(pos_x, pos_y, GetColor(0, 0, 0), "%s", strings_hitbox);//ヒットボックス内の文字を表示
 
 }
--- simProject/header/game_object/basic/in_game_window/game_window_cursor.h
+++ simProject/header/game_object/basic/in_game_window/game_window_cursor.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "DxLib.h"
+#include "FileInput.h"
 #include "Collision.h"
 
 class GameWindowCursor
@@ -9,7 +10,9 @@ private:
 
 public:
 
+	int char_size = 8;//文字の大きさ
+
 	void Init();
 	void Load();
-	void Draw(int pos_x, int pos_y, bool wire = false);
+	void Draw(int pos_x, int pos_y, char* strings_hitbox, char* strings, bool wire = false);//(x位置､y位置､ヒットボックス内の文字､カーソルウィンドウ内の文字､ワイヤー表示)
 };
