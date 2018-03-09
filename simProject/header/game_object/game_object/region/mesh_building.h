c04f2b8ffc6d63889c27fb055329f4895719bbe9
KouKeioh <kou2013kou.kou@gmail.com>
2018-03-10 04:38:10 +0900

    どうしよう

--- simProject/header/game_object/game_object/region/mesh_building.h
+++ simProject/header/game_object/game_object/region/mesh_building.h
@@ -9,12 +9,13 @@ private:
 public:
 	Building building;
 
-	int affiliation_faction;//所属しているファクション(0で無所属)
-	int building_type;//建物の種類
+	int affiliation_faction = 0;//所属しているファクション(0で無所属)
+	int building_type = 0;//建物の種類
 
 	void Init();
 	void Load();
 	void Transform(VECTOR scale, float rote_x, float rote_y, float rote_z, VECTOR pos);
+	void Updata();
 	void Draw(MODE mode, bool wire = false);
 	void DrawUI(int pos_x, int pos_y, bool wire = false);
 };
