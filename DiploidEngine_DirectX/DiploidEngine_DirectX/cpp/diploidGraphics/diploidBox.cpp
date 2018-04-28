bce29928424700639199c5f97a3b274779375e36
KouKeioh <kou2013kou.kou@gmail.com>
2018-04-29 05:31:45 +0900

    up

--- DiploidEngine_DirectX/DiploidEngine_DirectX/cpp/diploidGraphics/diploidBox.cpp
+++ DiploidEngine_DirectX/DiploidEngine_DirectX/cpp/diploidGraphics/diploidBox.cpp
@@ -12,5 +12,8 @@ void DiploidBox::Update()
 
 void DiploidBox::Draw(bool wire)
 {
-
+	if (wire == true)
+	{
+		DrawBoxAA(position.x, position.y, position.x + size.x, position.y + size.y, color, fill, thickness);
+	}
 }
