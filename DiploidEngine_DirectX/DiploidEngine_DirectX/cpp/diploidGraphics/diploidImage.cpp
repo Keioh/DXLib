fbaa2d7096b76fc2269a8c95b09e3d5eb5ea5c18
KouKeioh <kou2013kou.kou@gmail.com>
2018-05-18 03:08:54 +0900

    updata

--- DiploidEngine_DirectX/DiploidEngine_DirectX/cpp/diploidGraphics/diploidImage.cpp
+++ DiploidEngine_DirectX/DiploidEngine_DirectX/cpp/diploidGraphics/diploidImage.cpp
@@ -2,13 +2,13 @@
 
 void DiploidEngineImage::Load(const char* path)
 {
-
+	handl = LoadGraph(path);
 }
 
 void DiploidEngineImage::Draw(bool draw = true)
 {
 	if (draw == true)
 	{
-		
+		DrawGraph(position.x, position.y, handl, TRUE);
 	}
 }
--- DiploidEngine_DirectX/DiploidEngine_DirectX/header/diploidGraphics/diploidImage.h
+++ DiploidEngine_DirectX/DiploidEngine_DirectX/header/diploidGraphics/diploidImage.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "DxLib.h"
 #include "diploidGraphics\diploidImage.h"
 
 class DiploidEngineImage
@@ -6,6 +7,9 @@ class DiploidEngineImage
 private:
 
 public:
+	int handl;
+	VECTOR position;
+
 	void Load(const char* path);
 	void Draw(bool draw = true);
 };
